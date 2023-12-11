773d942735f78190037b90e412a4dd37c110dba1
taran <taran.s.lau@gmail.com>
2023-12-11 03:31:50 -0800

    next steps: 1. blender script can unzip model, take pictures, then delete model. 2. collect models will not try to download more than one model a second, it will also back off from rate limit warnings.

--- generate_data/generate_images_scratch.py
+++ generate_data/generate_images_scratch.py
@@ -1,12 +1,13 @@
+# looks like I will refactor this script into blender_script.py manipulate_file function to unzip model,
+# take and save pictures then eventually delete unzipped model once we are done with it
+
 import gzip
 import shutil
 import zipfile
 from pathlib import Path
 import yaml
 
-# try to use this to validate unzipped files
-from pygltflib import GLTF2
-from pygltflib.validator import validate, summary
+
 
 
 zipped_model_directory = 'getting_website_data/model_files'
@@ -20,9 +21,11 @@ for champion in champion_skin_mapping:
         zipped_file = Path(zipped_model_directory) / f"{champion['name']}" / f"{skin['name']}" / f"{champion['name']}.glb.gz"
         output_file = Path(model_image_directory) / f"{champion['name']}" / f"{skin['name']}" / f"{skin['name']}.glb"
 
-        print(zipped_file)
+        # print(zipped_file)
         # print(output_file)
+        # output_file.touch()
         if not zipped_file.is_file():
+            print(f"output file {output_file} does not exist")
             continue
 
         with gzip.open(zipped_file, 'rb') as f_in:
@@ -31,9 +34,6 @@ for champion in champion_skin_mapping:
             with open(output_file, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-        # gltf = GLTF2().load(output_file)
-        # validate(gltf)
-        # summary(gltf)
 
 
 
