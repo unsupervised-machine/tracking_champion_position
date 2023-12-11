497361a9ee4c1575aa46536e703a1b4b15107c1b
taran <taran.s.lau@gmail.com>
2023-12-11 03:15:25 -0800

    work work work

--- generate_data/generate_images_scratch.py
+++ generate_data/generate_images_scratch.py
@@ -4,16 +4,37 @@ import zipfile
 from pathlib import Path
 import yaml
 
+# try to use this to validate unzipped files
+from pygltflib import GLTF2
+from pygltflib.validator import validate, summary
 
-with open('getting_website_data/champion_skin_mapping_small.yaml', 'r') as yaml_file:
-    champion_skin_mapping = yaml.safe_load(yaml_file)
 
 zipped_model_directory = 'getting_website_data/model_files'
 model_image_directory = 'generate_data/images'
 
+with open('getting_website_data/champion_skin_mapping_small.yaml', 'r') as yaml_file:
+    champion_skin_mapping = yaml.safe_load(yaml_file)
+
 for champion in champion_skin_mapping:
-    zipped_file = Path(zipped_model_directory) / f"{champion['name']}"
-    print(zipped_file)
+    for skin in champion['skins']:
+        zipped_file = Path(zipped_model_directory) / f"{champion['name']}" / f"{skin['name']}" / f"{champion['name']}.glb.gz"
+        output_file = Path(model_image_directory) / f"{champion['name']}" / f"{skin['name']}" / f"{skin['name']}.glb"
+
+        print(zipped_file)
+        # print(output_file)
+        if not zipped_file.is_file():
+            continue
+
+        with gzip.open(zipped_file, 'rb') as f_in:
+            print(f_in)
+
+            with open(output_file, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+        # gltf = GLTF2().load(output_file)
+        # validate(gltf)
+        # summary(gltf)
+
 
 
 
